7e0768b827ac2d9c04a9aeb9f72e0fbfca275a82
Denick12 <catlynsmith036@gmail.com>
2025-09-30 04:44:13 +0300

    first commit

---
+++ kafka-consumer/consumer.py
@@ -0,0 +1,28 @@
+
+import time
+import json
+import psycopg2
+from kafka import KafkaProducer
+
+conn = psycopg2.connect("dbname=air_quality user=airflow password=airflow host=postgres_air port=5432")
+cursor = conn.cursor()
+
+producer = KafkaProducer(
+    bootstrap_servers="localhost:29092",
+    value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+)
+
+last_id = 0
+
+while True:
+    cursor.execute("SELECT * FROM air_quality WHERE id > %s ORDER BY id ASC", (last_id,))
+    rows = cursor.fetchall()
+    colnames = [desc[0] for desc in cursor.description]
+
+    for row in rows:
+        record = dict(zip(colnames, row))
+        producer.send("air_quality_topic", value=record)
+        last_id = record["id"]
+
+    producer.flush()
+    time.sleep(60)  # poll every minute
